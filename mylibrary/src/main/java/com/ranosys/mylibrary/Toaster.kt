1d35f279af532506402547a01587c9a46741b349
DESKTOP-JSAQHMN\User <ankur.upadhyay@ranosys.com>
2022-05-08 21:54:23 +0530

    Initial commit

---
+++ mylibrary/src/main/java/com/ranosys/mylibrary/Toaster.kt
@@ -0,0 +1,13 @@
+package com.ranosys.mylibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class Toaster {
+    companion object{
+        fun simpleToast(context:Context,msg:String)
+        {
+            Toast.makeText(context, "$msg", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
