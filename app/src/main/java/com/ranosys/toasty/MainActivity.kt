1e2a00d8d907c7ca17ee6feb5a356ea8e10a222e
DESKTOP-JSAQHMN\User <ankur.upadhyay@ranosys.com>
2022-05-08 22:14:14 +0530

    Test my own library

--- app/src/main/java/com/ranosys/toasty/MainActivity.kt
+++ app/src/main/java/com/ranosys/toasty/MainActivity.kt
@@ -2,12 +2,11 @@ package com.ranosys.toasty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ranosys.mylibrary.Toaster
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toaster.simpleToast(this,"Simple test ")
+
     }
 }
